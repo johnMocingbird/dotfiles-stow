aebeae13c075d890cbd721e0d6f98d1c9fc2207e
johnMocingbird <john.coakley@mocingbird.com>
2024-03-07 15:27:15 +0700

    added dooit to keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -180,6 +180,7 @@ map("n", "<leader>ub", function() Util.toggle("background", false, {"light", "da
 
 -- lazygit
 map("n", "<leader>gg", function() Util.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false }) end, { desc = "Lazygit (root dir)" })
+map("n", "<leader>dd", function() Util.terminal({ "dooit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false }) end, { desc = "Dooit" })
 map("n", "<leader>gG", function() Util.terminal({ "lazygit" }, {esc_esc = false, ctrl_hjkl = false}) end, { desc = "Lazygit (cwd)" })
 
 -- quit
