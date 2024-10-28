2288d99ce7617b48fb7c3d1d32846d9594e3b930
John <john.coakley@mocingbird.com>
2024-10-28 02:14:28 -0500

    created snippet for a fmt template

--- nvim/.config/nvim/lua/luasnippets/luasnip.lua
+++ nvim/.config/nvim/lua/luasnippets/luasnip.lua
@@ -34,12 +34,126 @@ local dynamic_flag_name = function()
 end
 
 ls.add_snippets("ruby", {
-	s("old-checkbox", {
-		f(dynamic_flag_name, {}),
-		i(1, "_checkbox_name: {"),
-		t(": {"),
+	s(
+		"task-sub-credits",
+		fmt(
+			[[
+      {{ # task {}
+        title: '{}',
+        description: '{}',
+        active_on: 0.months,
+        active_till: 0.months,
+        interval: {}.years,
+        cyclic: true,
+        identifier_under_specialty: '{}',
+        category_1_sub_credits_flag: %w[{}],
+        sub_credit_flags: {{
+          from: Certification,
+          flag_names: %w[{}]
+        }},
+        sub_credits: {}
+      }},
+      ]],
+			{
+				i(1, "Task_Cell"), -- User input for the task name (X)
+				i(2, "Title"), -- User input for the title (X),
+				i(3, "description"),
+				i(4, "0"), -- User input for the interval (X)
+				i(5, "0"), -- User input for identifier (X)
+				i(6, "md_do_credits"),
+				i(7, "FlagName"), -- note, need to have flag name for singe name flags for arry
+				i(8, "0"), --  NOTE: need to have sub_credits main_credits main_categorized_credits
+			}
+		)
+	),
+})
+
+-- TODO: add exemption clause
+ls.add_snippets("lua", {
+	s(
+		"",
+		fmt(
+			[[
+      {{
+
+      }},
+      ]],
+			{}
+		)
+	),
+})
+
+ls.add_snippets("lua", {
+	s(
+		"fmt_temp",
+		fmt(
+			[[
+      ls.add_snippets("{}", {{
+        s(
+          "{}",
+          fmt(
+            [[
+            {{{{
+              {}
+            }}}},
+            {}
+            {{}}
+            )
+          ),
+        }})
+      ]],
+			{
+				i(1, "language"),
+				i(2, "snippet_name"),
+				i(3, "snippet_body"),
+				t("]],"),
+			}
+		)
+	),
+})
+
+-- TODO: create a dynamic way to add flags
+
+ls.add_snippets("ruby", {
+	s("md_do_credits", {
+		t({
+			"%w[",
+			"  categorized_main_credits_category_1_ama_pra",
+			"  categorized_main_credits_category_2_ama_pra",
+			"  categorized_main_category_1_a_aoa",
+			"  categorized_main_category_1_b_aoa",
+			"  categorized_main_category_2_a_aoa",
+			"  categorized_main_category_2_b_aoa",
+			"  categorized_main_category_aafp_prescribed",
+			"  categorized_main_category_aafp_elective",
+			"  categorized_main_category_1_acog_cognate",
+			"  categorized_main_category_1_acep",
+			"  category_pharmacology_credits",
+			"]",
+		}),
 	}),
 })
+
+-- TODO: do something like this, to name the flag name, based on the name of the checkbox
+--   local function generate_slug(input)
+--   -- Convert to lowercase
+--   local result = string.lower(input)
+--
+--   -- Remove special characters
+--   result = result:gsub("[^%w%s]", "")
+--
+--   -- Replace spaces with underscores
+--   result = result:gsub("%s+", "_")
+--
+--   -- Truncate the string if it exceeds 40 characters
+--   local max_length = 40
+--   if #result > max_length then
+--     result = result:sub(1, max_length)
+--   end
+--
+--   return result
+-- end
+--
 
 ls.add_snippets("ruby", {
 	s(
@@ -65,36 +179,14 @@ ls.add_snippets("ruby", {
 				f(function(args)
 					return args[1][1]
 				end, { 1 }),
-				i(3, "display_value_key"),
+				c(3, {
+					t("Number of sub-credits"),
+					t("Number of Main Credits"),
+					i(nil, ""),
+				}),
 				t("display_type: 'input_field'"),
 				i(4, "category"),
 				i(5, "sub_category"),
-			}
-		)
-	),
-})
-
--- Example snippet for an object with multi-line support
-ls.add_snippets("lua", {
-	s(
-		"obj",
-		fmt(
-			[[
-    local {} = {{
-      key1 = {},
-      key2 = {},
-      key3 = {{
-        subkey1 = {},
-        subkey2 = {},
-      }},
-    }}
-  ]],
-			{
-				i(1, "object_name"),
-				i(2, "value1"),
-				i(3, "value2"),
-				i(4, "subvalue1"),
-				i(5, "subvalue2"),
 			}
 		)
 	),
