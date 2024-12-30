8d5e05a18cc50c34e454107b34ade4d53910aa57
John Coakley <john.coakley@mocingbird.com>
2024-12-30 16:44:07 +0700

    updates

--- yabai/.config/yabai/unfloat.sh
+++ yabai/.config/yabai/unfloat.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 apps_to_unfloat=("Safari" "Arc" "Terminal" "kitty" "Alacritty" "Slack" "ChatGPT")
 
 # Query all windows and unfloat those that match the apps and are currently floating
@@ -8,8 +7,8 @@ yabai -m query --windows | jq -c '.[]' | while read -r window; do
 	is_floating=$(echo "$window" | jq '.["is-floating"]')
 	window_title=$(echo "$window" | jq -r '.title')
 
-	# If the window belongs to one of the target apps, is floating, and the title is not "window-switcher", unfloat it
-	if [[ "$is_floating" == "true" && " ${apps_to_unfloat[*]} " == *" $app_name "* && "$window_title" != "window-switcher" ]]; then
+	# If the window belongs to one of the target apps, is floating, and the title is not "window-switcher" or "John", unfloat it
+	if [[ "$is_floating" == "true" && " ${apps_to_unfloat[*]} " == *" $app_name "* && "$window_title" != "window-switcher" && "$window_title" != "John" ]]; then
 		yabai -m window "$window_id" --toggle float
 		echo "Unfloated window $window_id of $app_name"
 	fi
